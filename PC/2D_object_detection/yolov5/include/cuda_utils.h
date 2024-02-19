66326e6d2f9bb7b2fdea4bf3b50ef0d6b62e4df0
csn <m18252039927@163.com>
2024-02-19 22:00:50 +0800

    1.完善部分预处代码

--- PC/2D_object_detection/yolov5/include/cuda_utils.h
+++ PC/2D_object_detection/yolov5/include/cuda_utils.h
@@ -13,10 +13,9 @@
 #define CUDA_CHECK(callstr)\
     {\
         cudaError_t error_code = callstr;\
-        if(error_code != cudaSuccess){ \
-            std::cerr << "CUDA error" << error_code << " at " << __FILE__ << " : " << __LINE__ ;\
+        if (error_code != cudaSuccess) {\
+            std::cerr << "CUDA error " << error_code << " at " << __FILE__ << ":" << __LINE__;\
             assert(0);\
-
         }\
     }
-#endif     // CUDA_CHECK
+#endif  // CUDA_CHECK
